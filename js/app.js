9030b6f33273881b6bec059a24367e6a2b19fc69
GSaucedo <g.saucedo.kd@gmail.com>
2021-09-20 02:20:33 -0500

    logic wrapped in functions

--- js/app.js
+++ js/app.js
@@ -1,8 +1,16 @@
-document
-  .getElementById("responsive-icon")
-  .addEventListener("click", function () {
-    document.getElementById("menu").classList.toggle("menu--mobile-visibility");
-  });
+function toggleResponsiveMenu() {
+  document
+    .getElementById("responsive-icon")
+    .addEventListener("click", function () {
+      document
+        .getElementById("menu")
+        .classList.toggle("menu--mobile-visibility");
+    });
+}
 
-document.getElementById("index-href").href = "index.html";
-document.getElementById("catalog-href").href = "products.html";
+function addLiks() {
+  document.getElementById("index-href").href = "index.html";
+  document.getElementById("catalog-href").href = "products.html";
+}
+addLiks();
+toggleResponsiveMenu();
