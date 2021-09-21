2a2dcc368e782e17988a56f86c8c819841fa6bd9
GSaucedo <g.saucedo.kd@gmail.com>
2021-09-20 21:47:47 -0500

    return html element intead of string

--- src/views/itemView.js
+++ src/views/itemView.js
@@ -2,16 +2,24 @@
  *@param product
  */
 function getItemView(product) {
-  return `
-        <section class="card display--flex flex-direction--column flex--center">
-          <img class="card__icon" src="${product.getImage()}" alt="lorem" />
-          <h2 class="card__title">
-            ${product.getQuantity()} ${product.getName()}
-          </h2>
-          <p class="card__description text--center">
-            ${product.getDescription()}
-          </p>
-          <p class="card__price">${product.getPrice()}</p>
-        </section>
-    `;
+  const template = document.createElement("section");
+  template.dataset.id = product.id;
+  template.classList.add(
+    "card",
+    "display--flex",
+    "flex-direction--column",
+    "flex--center",
+    "text--center"
+  );
+  template.innerHTML = `
+    <img class="card__icon" src="${product.getImage()}" alt="lorem" />
+    <h2 class="card__title">
+      ${product.getQuantity()} ${product.getName()}
+    </h2>
+    <p class="card__description text--center">
+      ${product.getDescription()}
+    </p>
+    <p class="card__price">${product.getPrice()}</p>
+`;
+  return template;
 }
