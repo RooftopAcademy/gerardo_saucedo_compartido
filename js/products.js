68660418694c522c64fdc21ce0f9f8e2b683d162
GSaucedo <g.saucedo.kd@gmail.com>
2021-09-20 21:50:51 -0500

    wrap logic in a function

--- js/products.js
+++ js/products.js
@@ -1,6 +1,14 @@
-const productsContainer = document.getElementById("products");
-const store = new Store();
-store.fetchProducts();
-store.getAllProducts().forEach((product) => {
-  productsContainer.innerHTML += getItemView(product);
-});
+function populateHTML() {
+  const productsContainer = document.getElementById("products");
+  const store = new Store();
+  store.fetchProducts();
+  store.getAllProducts().forEach((product) => {
+    const view = getItemView(product);
+    view.addEventListener("click", function () {
+      localStorage.setItem("id", this.dataset.id);
+      window.location.href = "detail.html";
+    });
+    productsContainer.appendChild(view);
+  });
+}
+populateHTML();
