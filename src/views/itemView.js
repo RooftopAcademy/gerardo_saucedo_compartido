85c751a6cb79ef0baf879a2728130fafe0c4ffbf
GSaucedo <g.saucedo.kd@gmail.com>
2021-09-20 21:22:56 -0500

    a tag removed

--- src/views/itemView.js
+++ src/views/itemView.js
@@ -2,23 +2,16 @@
  *@param product
  */
 function getItemView(product) {
-  return `<section class="card">
-          <a
-            class="
-              product__link
-              display--flex
-              flex-direction--column
-              flex--center
-            "
-            href="detail.html"
-          >
-            <img class="card__icon" src="${product.getImage()}" alt="lorem" />
-            <h2 class="card__title">${product.getQuantity()} ${product.getName()}</h2>
-            <p class="card__description text--center">
-              ${product.getDescription()}
-            </p>
-            <p class="card__price">${product.getPrice()}</p>
-          </a>
+  return `
+        <section class="card display--flex flex-direction--column flex--center">
+          <img class="card__icon" src="${product.getImage()}" alt="lorem" />
+          <h2 class="card__title">
+            ${product.getQuantity()} ${product.getName()}
+          </h2>
+          <p class="card__description text--center">
+            ${product.getDescription()}
+          </p>
+          <p class="card__price">${product.getPrice()}</p>
         </section>
     `;
 }
