2904b2c05e6b2f28c6b12d4c814eb23785860212
GSaucedo <g.saucedo.kd@gmail.com>
2021-09-20 20:18:00 -0500

    added example products

--- src/Store.js
+++ src/Store.js
@@ -5,7 +5,51 @@ class Store {
     this.user = new User();
   }
   fetchProducts() {
-    this.productsList.push(new Product());
+    this.productsList.push(
+      new Product(
+        0,
+        "Anti air trap",
+        "lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet",
+        144,
+        "../img/anti-air.png"
+      )
+    );
+    this.productsList.push(
+      new Product(
+        1,
+        "Broadside trap",
+        "lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet",
+        144,
+        "../img/broadside.png"
+      )
+    );
+    this.productsList.push(
+      new Product(
+        2,
+        "Ceiling drop trap",
+        "lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet",
+        144,
+        "../img/ceiling-drop-trap.png"
+      )
+    );
+    this.productsList.push(
+      new Product(
+        3,
+        "Ceiling electric field trap",
+        "lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet",
+        144,
+        "../img/ceiling-electric-field.png"
+      )
+    );
+    this.productsList.push(
+      new Product(
+        4,
+        "Ceiling gas trap",
+        "lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet",
+        144,
+        "../img/ceiling-gas-trap.png"
+      )
+    );
   }
   getAllProducts() {
     return this.productsList;
