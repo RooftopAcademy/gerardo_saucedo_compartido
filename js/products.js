58f389e7475f9af1ac6665ee1a539d2cc4c6b139
GSaucedo <g.saucedo.kd@gmail.com>
2021-09-20 20:16:50 -0500

    minor fix

--- js/products.js
+++ js/products.js
@@ -2,6 +2,5 @@ const productsContainer = document.getElementById("products");
 const store = new Store();
 store.fetchProducts();
 store.getAllProducts().forEach((product) => {
-  const view = getItemView(product);
-  productsContainer.innerHTML += view;
+  productsContainer.innerHTML += getItemView(product);
 });
