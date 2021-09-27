eef38de3381d87cb7390d76fee981fe09ab5d57a
GSaucedo <g.saucedo.kd@gmail.com>
2021-09-26 22:05:56 -0500

    typescript version of javascript file

---
+++ ts/detail.ts
@@ -0,0 +1,16 @@
+import { Store } from "../src/Store";
+import { getItemView } from "../src/views/itemView";
+
+function populateHTML() {
+  const productsContainer = document.getElementById("product-detail");
+  const store = new Store();
+  const id = new URLSearchParams(window.location.search).get("id");
+  store.fetchProducts().then((res) => {
+    store.setProducts(res);
+    const view = getItemView(
+      store.findProductById(id == null ? -1 : Number(id))
+    );
+    productsContainer.appendChild(view);
+  });
+}
+populateHTML();
