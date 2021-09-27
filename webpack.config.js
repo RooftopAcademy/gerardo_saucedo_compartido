04aee3fcb883238ad09c329d78243e70d398c47c
GSaucedo <g.saucedo.kd@gmail.com>
2021-09-26 21:05:40 -0500

    webpack copnfig

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  entry: {
+    index: ["./ts/app.ts", "./ts/index.ts"],
+    products: ["./ts/app.ts", "./ts/products.ts"],
+  },
+  output: {
+    filename: "./js/[name].js",
+  },
+  // Enable sourcemaps for debugging webpack's output.
+  devtool: "source-map",
+  resolve: {
+    // Add '.ts' and '.tsx' as resolvable extensions.
+    extensions: ["", ".webpack.js", ".web.js", ".ts", ".tsx", ".js"],
+  },
+  module: {
+    rules: [
+      // All files with a '.ts' or '.tsx' extension will be handled by 'ts-loader'.
+      { test: /\.tsx?$/, loader: "ts-loader" },
+      // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+      { test: /\.js$/, loader: "source-map-loader" },
+    ],
+  },
+  // Other options...
+  mode: "production",
+};
