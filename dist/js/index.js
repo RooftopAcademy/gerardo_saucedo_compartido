63ff962de6f3f68052fff588f0308ba80bc505cb
GSaucedo <g.saucedo.kd@gmail.com>
2021-09-26 20:45:38 -0500

    bundled files from each html page

---
+++ dist/js/index.js
@@ -0,0 +1,2 @@
+document.getElementById("index-href").href="index.html",document.getElementById("catalog-href").href="products.html",document.getElementById("responsive-icon").addEventListener("click",(function(){document.getElementById("menu").classList.toggle("menu--mobile-visibility")})),document.getElementById("banner-title").innerHTML="Save the world <br/> items shop";
+//# sourceMappingURL=index.js.map
